f1a3bae075d6d080359d2516409a914bc9761ced
x1ao4 <kazaf_ken@163.com>
2024-03-16 20:04:32 +0800

    增加跳过库功能，去除了交互填写配置信息的功能

--- plex-localization-zh/plex-localization-zh.py
+++ plex-localization-zh/plex-localization-zh.py
@@ -1,3 +1,5 @@
+# v1.1
+
 import sys
 import time
 from configparser import ConfigParser
@@ -5,8 +7,6 @@ import pypinyin
 import requests
 from pathlib import Path
 import concurrent.futures
-
-# 版本 1.0
 
 TAGS = {
     "Anime": "动画",
@@ -56,7 +56,7 @@ TAGS = {
     "Soap": "肥皂剧",
 }
 
-TYPE = {"movie": 1, "show": 2, "artist": 8, "album": 9, "track": 10, "photo":99} #99随手写的
+TYPE = {"movie": 1, "show": 2, "artist": 8, "album": 9, "track": 10, "photo":99}
 
 config_file: Path = Path(__file__).parent / 'config.ini'
 
@@ -89,32 +89,11 @@ class PlexServer:
         cfg = ConfigParser()
         self.s = requests.session()
 
-        try:
-            cfg.read(config_file)
-            self.host = dict(cfg.items("server"))["host"]
-            self.token = dict(cfg.items("server"))["token"]
-            print(f"已成功连接到服务器: {self.login()}\n")
-        except Exception as error:
-            print(error)
-            print("\n配置文件读取失败，开始创建配置文件...\n")
-            self.host = input('请输入您的 Plex 服务器地址（例如 http://127.0.0.1:32400）: ') or "http://127.0.0.1:32400"
-            if self.host[-1] == "/":
-                self.host = self.host[:-1]
-            self.token = input('请输入您的 X-Plex-Token: ')
-            try:
-                cfg.add_section("server")
-                cfg.set("server", "host", self.host)
-                cfg.set("server", "token", self.token)
-                with open(config_file, 'w') as f:
-                    cfg.write(f)
-                print(f"\n配置文件已写入 {config_file}\n")
-                self.host = self.host
-                self.token = self.token
-                print(f"已成功连接到服务器: {self.login()}\n")
-
-            except Exception as error:
-                print(error)
-                print("\n配置文件写入失败\n")
+        cfg.read(config_file)
+        self.host = dict(cfg.items("server"))["address"]
+        self.token = dict(cfg.items("server"))["token"]
+        self.skip_libraries = dict(cfg.items("server"))["skip_libraries"].split('；')
+        print(f"已成功连接到服务器: {self.login()}\n")
 
     def login(self):
         try:
@@ -126,21 +105,6 @@ class PlexServer:
             print("\n服务器连接不成功，请检查配置文件是否正确。\n")
             time.sleep(10)
             return sys.exit()
-
-    def select_library(self):
-        data = self.s.get(
-            url=f"{self.host}/library/sections/"
-        ).json().get("MediaContainer", {}).get("Directory", [])
-
-        library = [
-            "{}> {}".format(i, data[i]["title"])
-            for i in range(len(data))
-        ]
-        # print(data)
-        index = int(input("\n".join(library) + "\n请选择库: "))
-        action_key = data[index]['key']
-        action_type = int(input("\n1> 电影\n2> 节目\n8> 艺术家\n9> 专辑\n10> 单曲\n请选择要操作的类型: "))
-        return action_key, action_type
 
     def list_library(self):
         data = self.s.get(
@@ -155,11 +119,11 @@ class PlexServer:
         datas = response.get("MediaContainer", {}).get("Metadata", [])
     
         if not datas:
-            if select[1] == 8:  # 如果是艺术家（歌手）
+            if select[1] == 8:
                 print("歌手数: 0")
-            elif select[1] == 9:  # 如果是专辑
+            elif select[1] == 9:
                 print("专辑数: 0")
-            elif select[1] == 10:  # 如果是音轨
+            elif select[1] == 10:
                 print("音轨数: 0")
             else:
                 print("媒体数: 0")
@@ -167,11 +131,11 @@ class PlexServer:
     
         media_keys = [data["ratingKey"] for data in datas]
     
-        if select[1] == 8:  # 如果是艺术家（歌手）
+        if select[1] == 8:
             print(f"歌手数: {len(media_keys)}")
-        elif select[1] == 9:  # 如果是专辑
+        elif select[1] == 9:
             print(f"专辑数: {len(media_keys)}")
-        elif select[1] == 10:  # 如果是音轨
+        elif select[1] == 10:
             print(f"音轨数: {len(media_keys)}")
         else:
             print(f"媒体数: {len(media_keys)}")
@@ -195,7 +159,7 @@ class PlexServer:
         )
 
     def put_genres(self, select, rating_key, tag, addtag):
-        """变更流派标签。"""
+        # 变更流派标签
         if TAGS.get(tag):  
             # 获取当前的所有标签
             current_tags = [genre.get("tag") for genre in self.get_metadata(rating_key).get('Genre', {})]
@@ -214,7 +178,7 @@ class PlexServer:
             return res
 
     def put_styles(self, select, rating_key, tag, addtag):
-        """变更风格标签。"""
+        # 变更风格标签
         if TAGS.get(tag):  
             # 获取当前的所有标签
             current_tags = [style.get("tag") for style in self.get_metadata(rating_key).get('Style', {})]
@@ -233,7 +197,7 @@ class PlexServer:
             return res
 
     def put_mood(self, select, rating_key, tag, addtag):
-        """变更情绪标签。"""
+        # 变更情绪标签
         if TAGS.get(tag):  
             # 获取当前的所有标签
             current_tags = [mood.get("tag") for mood in self.get_metadata(rating_key).get('Mood', {})]
@@ -360,10 +324,10 @@ class PlexServer:
         library_list = self.list_library()
     
         for ll in library_list:
-            if ll[1] != 99:
+            if ll[1] != 99 and ll[2] not in self.skip_libraries:
                 select = ll[:2]
                 print(f"处理库: {ll[2]}")
-                if ll[1] == 8:  # 如果是音乐库
+                if ll[1] == 8:
     
                     # 处理艺术家（歌手）
                     artist_keys = self.list_media_keys([select[0], 8])
@@ -402,8 +366,9 @@ class PlexServer:
 
     def loop_all_collections(self):
         library_list = self.list_library()
+
         for ll in library_list:
-            if ll[1] != 99:
+            if ll[1] != 99 and ll[2] not in self.skip_libraries:
                 select = ll[:2]
                 print(f"处理库: {ll[2]}")
                 response = self.s.get(url=f'{self.host}/library/sections/{select[0]}/collections').json()
